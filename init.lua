1bcba4eedec96b1819b23d9887e3df304118a5ec
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2025-02-18 04:00:20 +0530

    feat: add nvim-tree config

--- init.lua
+++ init.lua
@@ -837,8 +837,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
-
+  -- { import = 'custom.plugins' },
   -- Nvimtree (File Explorer)
   -- Added this reference to the initial file
   {
@@ -847,11 +846,8 @@ require('lazy').setup({
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
-    config = function()
-      vim.keymap.set("n", "<leader>x", ":NvimTreeToggle<CR>", { silent = true })
-      require('lua.custom.plugins.nvim-tree-config')
-    end,
   },
+
 }, {
     ui = {
       -- If you have a Nerd Font, set icons to an empty table which will use the
@@ -877,6 +873,11 @@ require('lazy').setup({
 require('luasnip').filetype_extend('javascript', { 'javascriptreact' })
 
 require('luasnip').filetype_extend('javascript', { 'html' })
+
+-- Nvimtree (File Explorer)
+-- Added this reference to the initial file
+require('nvim-tree-config')
+vim.keymap.set("n", "<leader>x", ":NvimTreeToggle<CR>", { silent = true })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
