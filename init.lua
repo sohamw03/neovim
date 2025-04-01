99d7c239f70bce66bf44448227af12e4e43e99c8
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2025-04-01 23:01:25 +0530

    feat: add oil.nvim

--- init.lua
+++ init.lua
@@ -944,6 +944,36 @@ require('lazy').setup({
     version = '0.2.0',
     opts = {}, -- see Options
   },
+
+  -- Oil.nvim
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<C-l>'] = false,
+          ['<C-j>'] = false,
+          ['<C-k>'] = false,
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+    -- Optional dependencies
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
